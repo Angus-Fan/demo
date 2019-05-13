656ed07d059d5cfcdd64eb448ba6f47e242299f5
angusfanSoftware <angusis@hotmail.ca>
2019-05-13 17:20:50 -0400

    - final commit with working code

---
+++ demo/src/main/java/com/JunitDemo/demo/additionClass.java
@@ -0,0 +1,14 @@
+package com.JunitDemo.demo;
+
+public class additionClass {
+	
+	public int addOne(int x) {
+		return x+1;
+	}
+	
+	public int addTwoNumbers(int x,int y) {
+		return x+y;
+	}
+
+
+}
